2edb241f77b4a0d2085b2fc1b08b931515a3496d
DanielAlonso2001 <155383012+DanielAlonso2001@users.noreply.github.com>
2024-06-02 21:01:17 -0500

    Reapply "Initial commit"

---
+++ app/src/main/java/com/example/stores2/LoginActivity.kt
@@ -0,0 +1,15 @@
+import com.example.stores2.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Implement registration logic here
+    }
+}
---
+++ app/src/main/java/com/example/stores2/RegisterActivity.kt
@@ -0,0 +1,15 @@
+import com.example.stores2.R
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        // Implement registration logic here
+    }
+}
